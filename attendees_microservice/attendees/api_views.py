176ad4f85fe024a7ce510b94f170b3cb3d59e793
Phat <ptnguyen728@gmail.com>
2023-06-28 08:53:23 -0500

    microservices complete

---
+++ attendees_microservice/attendees/api_views.py
@@ -0,0 +1,71 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+# from events.api_views import ConferenceListEncoder
+from common.json import ModelEncoder
+from .models import Attendee, ConferenceVO
+# from events.models import Conference
+
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
+class AttendeeListEncoder(ModelEncoder):
+    model = Attendee
+    properties = ["name"]
+
+
+class AttendeeDetailEncoder(ModelEncoder):
+    model = Attendee
+    properties = [
+        "email",
+        "name",
+        "company_name",
+        "created",
+        "conference",
+    ]
+    encoders = {
+        "conference": ConferenceVODetailEncoder(),
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_attendees(request, conference_vo_id):
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        return JsonResponse(
+            {"attendees": attendees},
+            encoder=AttendeeListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        # Get the Conference object and put it in the content dict
+        try:
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(import_href=conference_href)
+            content["conference"] = conference
+        except ConferenceVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+
+
+def api_show_attendee(request, pk):
+    attendee = Attendee.objects.get(id=pk)
+    return JsonResponse(
+        attendee,
+        encoder=AttendeeDetailEncoder,
+        safe=False,
+    )
--- attendees_microservice/attendees_bc/urls.py
+++ attendees_microservice/attendees_bc/urls.py
@@ -1,7 +1,8 @@
-"""conference_go URL Configuration
+"""
+URL configuration for attendees_bc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("attendees.api_urls")),
-    path("api/", include("events.api_urls")),
-    path("api/", include("presentations.api_urls")),
 ]
